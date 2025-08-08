79d7595f447c6cc74d0a21a5ba2080df9b34b3c9
Vladimir Taburkin <09062004v@gmail.com>
2025-08-09 01:18:32 +0300

    init basic logger

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -1,18 +1,47 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
+	"os"
 
 	"github.com/VACdotCS/kaban-go-auth-service/internal/config"
 )
 
+const (
+	envLocal = "local"
+	envDev   = "envDev"
+	envProd  = "prod"
+)
+
 func main() {
-	// TODO: init config
 	cfg := config.MustLoad()
-	fmt.Println(cfg)
 	// TODO: logger init
+
+	log := setupLogger(cfg.Env)
+	log.Info("Starting application")
 
 	// TODO: init app
 
 	// TODO: run gRPC-server of the app
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
 }
